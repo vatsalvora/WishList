1732d470af2883089a5b4f69a02d0764d0d23369
Joon Kim <jkim91@ufl.edu>
2014-01-29 13:10:53 -0500

    removing crap

--- WishListUI/WishList/gen/com/wishlist/ui/R.java
+++ WishListUI/WishList/gen/com/wishlist/ui/R.java
@@ -24,7 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int emailInput=0x7f080001;
+        public static final int emailText=0x7f080002;
+        public static final int login=0x7f080000;
+        public static final int passwordInput=0x7f080004;
+        public static final int passwordText=0x7f080003;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
@@ -35,6 +40,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int email=0x7f05000f;
         public static final int friendlist=0x7f05000b;
         public static final int hello_world=0x7f050002;
         public static final int item=0x7f05000e;
@@ -45,6 +51,7 @@ public final class R {
         public static final int name=0x7f050007;
         public static final int notes=0x7f050009;
         public static final int options=0x7f05000d;
+        public static final int password=0x7f050010;
         public static final int price=0x7f050008;
         public static final int redo=0x7f050006;
         public static final int undo=0x7f050005;

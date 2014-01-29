cd5becbad8a114ab805104679ed9a082ef930d32
Joon Kim <jkim91@ufl.edu>
2014-01-29 13:53:32 -0500

    updating values in UI

--- WishListUI/WishList/gen/com/wishlist/ui/R.java
+++ WishListUI/WishList/gen/com/wishlist/ui/R.java
@@ -24,7 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int listOfPeople=0x7f080001;
+        public static final int listView1=0x7f080002;
+        public static final int name_of_owner=0x7f080000;
     }
     public static final class layout {
         public static final int activity_wish_list_view=0x7f030000;
@@ -35,7 +38,24 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int email=0x7f05000f;
+        public static final int friendlist=0x7f05000b;
         public static final int hello_world=0x7f050002;
+        public static final int item=0x7f05000e;
+        public static final int list=0x7f050004;
+        public static final int login=0x7f050003;
+        public static final int logout=0x7f05000c;
+        public static final int mylist=0x7f05000a;
+        public static final int name=0x7f050007;
+        public static final int notes=0x7f050009;
+        public static final int options=0x7f05000d;
+        public static final int password=0x7f050010;
+        public static final int people=0x7f050012;
+        public static final int price=0x7f050008;
+        public static final int redo=0x7f050006;
+        public static final int title_activity__wishlist_view=0x7f050011;
+        public static final int title_activity_people_list=0x7f050013;
+        public static final int undo=0x7f050005;
     }
     public static final class style {
         /** 

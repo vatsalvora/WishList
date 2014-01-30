7a860c3a1cfbf3f8e9fd7382d2273473ba95a209
Joon Kim <jkim91@ufl.edu>
2014-01-29 19:01:10 -0500

    Added WishItem in UI.

--- WishListUI/WishList/gen/com/wishlist/ui/R.java
+++ WishListUI/WishList/gen/com/wishlist/ui/R.java
@@ -26,8 +26,8 @@ public final class R {
     public static final class id {
         public static final int action_settings=0x7f080003;
         public static final int listOfPeople=0x7f080001;
-        public static final int listView1=0x7f080002;
         public static final int name_of_owner=0x7f080000;
+        public static final int wishlist=0x7f080002;
     }
     public static final class layout {
         public static final int activity_wish_list_view=0x7f030000;
--- WishListUI/WishList/src/com/wishlist/ui/WishListView.java
+++ WishListUI/WishList/src/com/wishlist/ui/WishListView.java
@@ -1,15 +1,37 @@
 package com.wishlist.ui;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class WishListView extends Activity {
 
+	//Android components
+	private TextView tv;
+	private ListView lv;
+	private Button b;
+	
+	//name of owner of list
+	private String currentOwner;
+	
+	//list to hold WishItems
+	private ArrayList<WishItem> wList;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wish_list_view);
+        
+        tv = (TextView) findViewById(R.id.name_of_owner);
+        lv = (ListView) findViewById(R.id.wishlist);
+        b = (Button) findViewById(R.id.listOfPeople);
+        
+        tv.setText(currentOwner);
     }
 
 

7330badcdc57df41f06e5c9ea40dd9805e58bbdb
vatsalvora <vatsal1@msn.com>
2014-02-02 23:05:11 -0500

    Facebook Login

--- WishListUI/WishList/src/com/wishlist/ui/Login.java
+++ WishListUI/WishList/src/com/wishlist/ui/Login.java
@@ -4,20 +4,45 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import com.facebook.*;
+import com.facebook.model.*;
+import android.widget.TextView;
+import android.content.Intent;
 
 public class Login extends Activity {
-
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
-	}
+	  public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_login);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login, menu);
-		return true;
-	}
+	    // start Facebook Login
+	    Session.openActiveSession(this, true, new Session.StatusCallback() {
 
-}
+	      // callback when session changes state
+	      @Override
+	      public void call(Session session, SessionState state, Exception exception) {
+	        if (session.isOpened()) {
+
+	          // make request to the /me API
+	          Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
+
+	            // callback after Graph API response with user object
+	            @Override
+	            public void onCompleted(GraphUser user, Response response) {
+	              if (user != null) {
+	                TextView welcome = (TextView) findViewById(R.id.welcome);
+	                welcome.setText("Hello " + user.getName() + "!");
+	              }
+	            }
+	          });
+	        }
+	      }
+	    });
+	  }
+
+	  @Override
+	  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	      super.onActivityResult(requestCode, resultCode, data);
+	      Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+	  }
+
+	}

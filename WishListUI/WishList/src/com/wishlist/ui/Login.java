88ff8b9092457d8a9cc2aa1eaffdfa5e659ef0f7
chy <sylvantier@gmail.com>
2014-02-05 16:02:31 -0500

    UI Changes 02/5/2014
    - Login:
    -  Now starts the main activity (with the user's wish list and friends
    list) after a user logs in
    - Removed the view (it was redundant and clunky-looking; the code
    automatically brings up facebook's login if the user isn't logged in)
    
    - Main Activity:
    - Made a main activity to hold tabs for the user's wish list and friends
    list
    - Made fragments for the user's wish list and friends list
    
    Logo:
    - created logo and saved it in the appropriate dpis for different screen
    resolutions
    - added the logo to the app
    
    TODO:
    - flesh out fragments with the stuff Joon did

--- WishListUI/WishList/src/com/wishlist/ui/Login.java
+++ WishListUI/WishList/src/com/wishlist/ui/Login.java
@@ -2,9 +2,10 @@ package com.wishlist.ui;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 import com.facebook.*;
 import com.facebook.model.*;
+
+import android.util.Log;
 import android.widget.TextView;
 import android.content.Intent;
 
@@ -12,26 +13,31 @@ public class Login extends Activity {
 	@Override
 	  public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.activity_login);
-
+//	    setContentView(R.layout.activity_login);
+	    /* We don't need an actual layout here; if the user is not logged in the
+	    *  code below automatically brings up the facebook login anyway. If the user is
+	    * already logged in, it brings up the main activity. 
+	    */
+	    
 	    // start Facebook Login
 	    Session.openActiveSession(this, true, new Session.StatusCallback() {
 
 	      // callback when session changes state
-	      @Override
+	      @SuppressWarnings("deprecation")
+		@Override
 	      public void call(Session session, SessionState state, Exception exception) {
 	        if (session.isOpened()) {
-
+	        	Log.i("MyActivity", "MyClass.getView() — get item number ");
 	          // make request to the /me API
 	          Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
-
+	        	
 	            // callback after Graph API response with user object
 	            @Override
 	            public void onCompleted(GraphUser user, Response response) {
-	              if (user != null) {
-	                TextView welcome = (TextView) findViewById(R.id.welcome);
-	                welcome.setText("Hello " + user.getName() + "!");
-	              }
+	             /* if (user != null) {
+	                	TextView welcome = (TextView) findViewById(R.id.welcome);
+	                	welcome.setText("Hello " + user.getName() + "!");
+	              } */
 	            }
 	          });
 	        }
@@ -43,6 +49,9 @@ public class Login extends Activity {
 	  public void onActivityResult(int requestCode, int resultCode, Intent data) {
 	      super.onActivityResult(requestCode, resultCode, data);
 	      Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+	      //Start the main activity
+	      Intent intent = new Intent(Login.this, MainActivity.class);
+    	  startActivity(intent);
 	  }
 
 	}

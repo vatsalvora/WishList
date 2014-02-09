82e343e1d5886038d4507b99559e5a5967648a58
PCieslewski <pawel.vip@gmail.com>
2014-02-08 22:23:09 -0500

    Initial DB Commit
    This is the first commit of the database portion of wishlist. This is a
    test DB android application.

---
+++ WishListDB/HelloPostGres/src/com/example/hellopostgres/DBCom.java
@@ -0,0 +1,39 @@
+//This file provides the libraries for using the WishList database. It will abstract the SQL, by creating simple java
+//methods that will integrate with the final product.
+
+package com.example.hellopostgres; //This line is for android functionality only. This is a test address. Change required based on final path.
+import java.sql.*;
+
+public class DBCom {
+	
+	String dburl = "jdbc:postgresql://98.180.57.56:5432/wishlist";
+	String username = "wishlist_app";
+	String password = "wl_app";
+	String output = "";
+	
+	public String test() throws Exception{
+		
+		Class.forName("org.postgresql.Driver");
+		
+		DriverManager.setLoginTimeout(5);
+		Connection connect = DriverManager.getConnection(dburl,username,password);
+		Statement st = connect.createStatement();
+		ResultSet resultSet = st.executeQuery("SELECT * FROM users");
+
+		
+		while (resultSet.next()){
+			for(int i = 1;i<3;i++){
+				output = output + (resultSet.getString(i) + " ");
+			}
+			output = output + "\n";
+		}
+		
+		resultSet.close();
+        st.close();
+        connect.close();
+		
+		return output;
+
+	}	
+
+}
---
+++ WishListDB/HelloPostGres/src/com/example/hellopostgres/MainActivity.java
@@ -0,0 +1,40 @@
+//This file is used only for DATABASE TESTING PURPOSES. This file will not be intgrated
+//into the WishList final product.
+
+package com.example.hellopostgres;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    TextView resultArea;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        resultArea = new TextView(this);
+        resultArea.setText("Please wait.");
+        setContentView(resultArea);
+        
+        new FetchSQL().execute();
+    }
+    
+    private class FetchSQL extends AsyncTask<Void,Void,String> {
+        @Override
+        protected String doInBackground(Void... params) {
+        	
+        	DBCom mydb = new DBCom();
+            try {
+				return mydb.test();
+			} catch (Exception e) {
+				return e.toString();
+			}
+        }
+        
+        @Override
+        protected void onPostExecute(String value) {
+            resultArea.setText(value);
+        }
+    }
+}

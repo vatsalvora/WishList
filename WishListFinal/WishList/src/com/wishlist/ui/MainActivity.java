a62a9a792df56dd34ebe930f99d52ac79e658da9
Joon Kim <jkim91@ufl.edu>
2014-02-27 14:13:50 -0500

    Unsaved changes.

--- WishListFinal/WishList/src/com/wishlist/ui/MainActivity.java
+++ WishListFinal/WishList/src/com/wishlist/ui/MainActivity.java
@@ -11,7 +11,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 //import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
-import android.view.*;
 import com.wishlist.db.*;
 import com.wishlist.obj.User;
 //import android.widget.TextView;

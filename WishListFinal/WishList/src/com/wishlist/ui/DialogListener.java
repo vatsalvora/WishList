aa9c375e2c9e6566a0a4d8dd023483e937cdb55b
Joon Kim <imjk91@gmail.com>
2014-03-17 15:08:31 -0400

    Working on editing a wish item.

--- WishListFinal/WishList/src/com/wishlist/ui/DialogListener.java
+++
@@ -1,9 +0,0 @@
-package com.wishlist.ui;
-
-import android.support.v4.app.DialogFragment;
-
-public interface DialogListener {
-	void onDialogPositiveClick(DialogFragment dialog);
-	void onDialogNegativeClick(DialogFragment dialog);
-	void onDialogNeutralClick(DialogFragment dialog);
-}
